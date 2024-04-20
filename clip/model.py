97bab645d0ac792472ed56554a9581b2a90f25e2
juveb7 <juveb6@gmail.com>
2024-04-20 15:16:48 -0700

    added clipfdsafdsa?

--- clip/model.py
+++ clip/model.py
@@ -341,10 +341,10 @@ class CLIP(nn.Module):
         return self.visual(image.type(self.dtype))
     
     def encode_text(self, text, local=False):
-	x = self.token_embedding(text).type(self.dtype)  # [batch_size, n_ctx, d_model]
+        x = self.token_embedding(text).type(self.dtype)  # [batch_size, n_ctx, d_model]
 
-	x = x + self.positional_embedding.type(self.dtype)
-	x = x.permute(1, 0, 2)  # NLD -> LND
+        x = x + self.positional_embedding.type(self.dtype)
+        x = x.permute(1, 0, 2)  # NLD -> LND
         x = self.transformer(x)
         x = x.permute(1, 0, 2)  # LND -> NLD
         x = self.ln_final(x).type(self.dtype)
